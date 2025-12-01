2d35163f5c176b5c841ba3cb4d0ccdcdda38e19f
JollyJames-Chidiebere <brightjollyjames@gmail.com>
2025-12-01 16:41:30 -0500

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run release command (migrations)
-echo "Running migrations..."
-python manage.py migrate --noinput
-
-# Start gunicorn with PORT from environment
-echo "Starting gunicorn on port $PORT..."
-exec gunicorn Breath_Pacer_Backend.wsgi:application --bind 0.0.0.0:${PORT:-8000}
