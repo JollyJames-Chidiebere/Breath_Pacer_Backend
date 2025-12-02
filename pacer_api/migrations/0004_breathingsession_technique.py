8caa18de11244c4986e68eebb5f2a82b25e3ef58
JollyJames-Chidiebere <brightjollyjames@gmail.com>
2025-12-02 01:23:29 -0500

    Create 0004_breathingsession_technique.py

---
+++ pacer_api/migrations/0004_breathingsession_technique.py
@@ -0,0 +1,17 @@
+# Generated migration for adding technique field
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pacer_api', '0003_breathplan_exhale_hold_ms'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='breathingsession',
+            name='technique',
+            field=models.CharField(blank=True, default='Breathing 2(Oscilloscope)', max_length=128),
+        ),
